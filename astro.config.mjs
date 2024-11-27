fc24b8883096e64309fbaa9f046befd867f87618
minhvip08 <minhvip08@gmail.com>
2024-11-27 15:36:39 +0700

    change url

--- astro.config.mjs
+++ astro.config.mjs
@@ -31,7 +31,7 @@ const oklchToHex = str => {
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://blog.simi.ovh',
+  site: 'https://minhvip08.github.io',
   base: '/',
   trailingSlash: 'always',
   integrations: [
